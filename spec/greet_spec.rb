ce9b2e403364003c81d28726110f2722c543031a
Ira Bitsola <ira.bitsola@yahoo.com>
2022-11-10 16:55:29 +0000

    added test on greet_spec

--- spec/greet_spec.rb
+++ spec/greet_spec.rb
@@ -1,8 +1,12 @@
 require 'greet'
 
 RSpec.describe "greet method" do
-    it "returns hello Ira when given the name Ira" do
+    it "returns 'Hello, Ira!' when given the name Ira" do
         result = greet("Ira")
         expect(result).to eq "Hello, Ira!"
+    end
+    it "returns 'Hello, Samir!' when given the name Samir" do
+        result = greet("Samir")
+        expect(result).to eq "Hello, Samir!"
     end
 end
